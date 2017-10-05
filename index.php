f4f9733bdc68fecf69fac14827f3cd259aefdfe8
gdm-1718-aurecard <aurecard@student.arteveldehs.be>
2017-10-05 10:30:48 +0200

    godver

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$str1 = "Hello";
+$str2 = "World";
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+<h3><?php echo $str1 ?> <?php echo $str2 ?></h3>
+
+</body>
+</html>
