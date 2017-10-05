0c6075d7b01fb9b322a77137c22fe0501f882edc
gdm-1718-aurecard <aurecard@student.arteveldehs.be>
2017-10-05 10:32:25 +0200

    oefening 2

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $str2 = "World";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>oefening2</title>
 </head>
 <body>
     
